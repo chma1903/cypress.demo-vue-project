180094c1ce485e29ee61f46086957c57ef71de73
Chunmei <wangchunmei1987@gmail.com>
2020-10-21 11:31:55 +0200

    Created the first test case

--- frontend-tests/cypress/integration/regression-test-suite.spec.js
+++ frontend-tests/cypress/integration/regression-test-suite.spec.js
@@ -7,6 +7,13 @@ describe('Regression test suite',function(){
     //this is a test case
     it('Perform valid login',function(){
         cy.visit('http://localhost:3000')
+        cy.title().should('include','Hotel')
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Tester Hotel Overview')
+        cy.get('.user > .btn').click()
+        cy.contains('Login')
     })
 
 })
